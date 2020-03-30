0dbe9138f5e64505c3d5adc9b00a0db44bd017be
gitrust <weebac@web.de>
2020-03-30 08:07:36 +0200

    sections 3 and 6

--- scp.py
+++ scp.py
@@ -3,7 +3,11 @@ import sys
 
 
 def p(p1,p2):
-  print(p1 + '\t' + str(p2))
+  print(p1.ljust(15,' ') + '\t\t' + str(p2))
+
+# bytes to int
+def b2i(bytes):
+  return int.from_bytes(bytes,'little')
 
 # 10
 class ScpPointer:
@@ -22,11 +26,27 @@ class ScpHeader:
     self.protnr = reader.readint(1)
     self.reserved = reader.reads(6)
 
+class PatientAgeFormat:
+  def __init__(self,bytes):
+    if bytes and len(bytes) > 2:
+      self.text = b2i(bytes[0:2])
+    else:
+      self.text = ''
+      
+class DateOfBirthFormat:
+  def __init__(self,bytes):
+    if bytes and len(bytes) > 2:
+      self.text = '{0}/{1}/{2}'.format(b2i(bytes[0:2]),b2i(bytes[2:3]),b2i(bytes[3:4]))
+    else:
+      self.text = ''
+    
 class Tag:
-  def __init__(self,reader):
+  def __init__(self,reader):  
     self.tag = reader.readint(1)
     self.len = reader.readint(2)
-    self.data = reader.reads(self.len)
+    self.data = None
+    if self.len > 0:
+      self.data = reader.read(self.len) 
     
 class ScpReader:
   def __init__(self,filename):
@@ -83,44 +103,108 @@ class Section1:
     self.t = []
     datalen = self.h.len - 16
     start = datalen
+    
+    # all tags in section
     while (start > 0):      
       tag = Tag(reader)
       start = start - tag.len
       self.t.append(tag)
-      
+  
+  def format_tag(self,tag):
+    for _tag in self.t:
+      if _tag.tag == tag:
+        if tag == 4:
+          return _tag.data[0:2].decode('iso-8859-1')
+        return _tag.data.decode('iso-8859-1')
+    return ''
     
+  def tag_data(self,idx):
+    for _tag in self.t:
+      if _tag.tag == idx:
+        return _tag.data
+        
+    return None
+
+# lead identification
+class Section3:
+  def __init__(self,pointer,reader):
+    reader.move(pointer.index - 1)
+    
+    self.h = ScpHeader(reader)
+    
+# rythm data
+class Section6:
+  def __init__(self,pointer,reader):
+    reader.move(pointer.index - 1)
+    
+    self.h = ScpHeader(reader)    
     
 def read_scp(f):
   scp = ScpReader(f)
   
+  p('--ScpRec--','----')
   p('CRC', scp.readint(2))
-  p('RecordLength' , scp.readint(4))
-  
-  s0 = Section0(scp)
-  
+  p('RecLen' , scp.readint(4))
   
   print()
 
   # section 0
-  p('S0 CRC', s0.h.crc)
-  p('S0 Id:' , s0.h.id)
-  p('S0 Len' , str(s0.h.len))
-  p('S0 VerNr' , s0.h.versnr)
-  p('S0 ProNr' , s0.h.protnr)
-  p('S0 Res', s0.h.reserved)
-  p('S0 pointers', len(s0.p))
+  s0 = Section0(scp)
+  p('--Section0--','----')
+  p('CRC', s0.h.crc)
+  p('Id:' , s0.h.id)
+  p('Len' , str(s0.h.len))
+  p('VerNr' , s0.h.versnr)
+  p('ProNr' , s0.h.protnr)
+  p('Res', s0.h.reserved)
+  p('pointers', len(s0.p))
   print()
   
+  p('--Section1--','----')
   s1 = Section1(s0.p[1],scp)
-  p('S1 CRC', s1.h.crc)
-  p('S1 Id:' , s1.h.id)
-  p('S1 Len' , str(s1.h.len))
-  p('S1 VerNr' , s1.h.versnr)
-  p('S1 ProNr' , s1.h.protnr)
-  p('S1 Res', s1.h.reserved)
-  p('S1 Tags', len(s1.t))
+  p('CRC', s1.h.crc)
+  p('Id:' , s1.h.id)
+  p('Len' , str(s1.h.len))
+  p('VerNr' , s1.h.versnr)
+  p('ProNr' , s1.h.protnr)
+  p('Res', s1.h.reserved)
+  p('Tags', len(s1.t))  
+  p('FirstName', s1.format_tag(1))
+  p('LastName', s1.format_tag(0))
+  p('Pat Id', s1.format_tag(2))
+  p('LastName(2)', s1.format_tag(3))
+  p('Age', s1.format_tag(4))
+  p('DateOfBirth',DateOfBirthFormat(s1.tag_data(5)).text)
+  p('Drugs', s1.format_tag(10))
+  p('DeviceId', s1.format_tag(14))
+  p('TExt', s1.format_tag(30))
+  p('EcgSeq', s1.format_tag(31))
+  p('MedHistory', s1.format_tag(35))
+
+  print()
+  p('--Section3--','----')
+  s3 = Section3(s0.p[3],scp)
+  p('CRC', s3.h.crc)
+  p('Id:' , s3.h.id)
+  p('Len' , str(s3.h.len))
+  p('VerNr' , s3.h.versnr)
+  p('ProNr' , s3.h.protnr)
+  p('Res', s3.h.reserved)
+  
+  print()
+  p('--Section6--','----')
+  s6 = Section6(s0.p[6],scp)
+  p('CRC', s6.h.crc)
+  p('Id:' , s6.h.id)
+  p('Len' , str(s6.h.len))
+  p('VerNr' , s6.h.versnr)
+  p('ProNr' , s6.h.protnr)
+  p('Res', s6.h.reserved)
   scp.close()
 
+
+
+  
 def main():
   f = sys.argv[1]
   
