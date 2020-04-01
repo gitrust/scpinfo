81fa466b1bd0b5ef69b9b04dc26792547f39dda3
gitrust <weebac@web.de>
2020-04-01 08:29:42 +0200

    Declare source encoding

--- scp.py
+++ scp.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # container for all SCP sections
 class ScpRecord:
--- scputil.py
+++ scputil.py
@@ -1,4 +1,5 @@
-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # bytes to int
 def b2i(bytes):
